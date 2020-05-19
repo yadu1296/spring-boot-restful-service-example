a82de1ae61ebd0bc60551af73f11513c4b39ff07
G Yadagiri <g.yadagiri@ivycomptech.com>
2020-05-19 17:02:47 +0530

    initial commit

---
+++ src/main/java/com/learntechstack/springrest/SpringBootRestfulServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.learntechstack.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulServiceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulServiceExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learntechstack/springrest/SpringBootRestfulServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learntechstack.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulServiceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
